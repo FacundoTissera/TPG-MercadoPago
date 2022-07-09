2b08bc299bd980904ad1b75db780877d947ff85e
Facundo <facundotisserasorribas@gmail.com>
2022-07-09 07:51:31 -0300

    Logica y funcionabilidad de implementacion de mercadopago

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,28 +1,67 @@
 const {index,one} = require('../models/product.model');
 
 module.exports ={
+
+
     index: (req,res) => res.render('home',{
         products:index(),
         styles:['home']
     }),
+
     // Step 3
+    // agregar producto al carrito creado
     addCart: (req,res) => {
+
         // Find Product in DB
-        // let product = ???
+        // elid viene de la vista
+        let product = one(req.body.id);
         // Check product exist in cart
-        // Case 1: Exist and update quantity
-        // Case 2: Add cart and set quantity
-        return res.send("Add a new product")
+        if (req.session.cart.find(item => item.id === product.id)) {
+            // if exist, update quantity
+            // Case 1: Exist and update quantity
+                req.session.cart = req.session.cart.map(item => {
+                    if (item.id === product.id) {
+                        item.quantity++;
+                    }
+                    return item;
+                });
+
+        }else{
+            // if not exist, add product to cart
+            // Case 2: Add cart and set quantity
+            req.session.cart.push({...product, quantity: 1});
+        }
+        return res.redirect("/")
     },
+
+
+
     // Step 5
     updateCart: (req,res) => {
         // Check quantity
+        if (req.body.quantity <= 0) {
+            // if quantity is 0, remove product from cart
+            req.session.cart = req.session.cart.filter(item => item.id != req.body.id);
+        }else{
+            // if quantity is not 0, update quantity
+            req.session.cart = req.session.cart.map(item => {
+                if (item.id == req.body.id) {
+                    item.quantity = req.body.quantity;
+                }
+                return item;
+            });
+        }
         // Case 1: Is equal to zero then remove product
         // Case 2: Update all cart items setting quantity in product selected
-        return res.send("Update quantity")
+        return res.redirect("/")
     }, 
     // Step 6
     removeCart: (req,res) =>{
-        return res.send("Remove a product from the cart")
+
+        // Remove product from cart
+        req.session.cart = req.session.cart.filter(item => item.id != req.body.id);
+        return res.redirect("/")
+
+        
     }
 }
--- modules/mercadoPago.js
+++ modules/mercadoPago.js
@@ -1,18 +1,43 @@
 // Step 7
 
-// const ??? = require(???)
-// const credential = ??? 
-// let server = ???
-// const success = ???
-// const failure = ???
-// const pending = ???
-/*
-const mp  = async (???,???,???) => {
+const mercadoPago = require('mercadopago');
+const credential =  process.env.MP || "TEST-3148667476802115-070817-4b5feae80c7a746772298554835c99b9-169752048";
+ let server = process.env.SERVER || "https://localhost:3030";
+ const feedback = `${server}/checkout/feedback`;
+ const failure = `${server}/checkout/feedback`;
+ const pending = `${server}/checkout/feedback`;  
+
+const mp  = async (items, cuotes, shipping ) => {
     try {
         // Magic
+        mercadoPago.configure({
+            access_token: credential
+        })
+        let config = {
+            items: items,
+            back_urls: {
+                success: feedback,
+                failure: failure,
+                pending: pending
+            },
+            payment_methods: {
+                // cuotas
+                installments: cuotes
+            },
+            auto_return: "approved",
+            shipments:{
+                cost: shipping,
+                mode: "not_specified",
+            }
+
+        }
+
+            let preference = await mercadoPago.preferences.create(config);
+
+            return preference;
+
     } catch (error) {
         throw new Error(error)
     }
 }
 module.exports = mp
-*/
